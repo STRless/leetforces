5c078f7f6ed1e29cd67ee98a6c0f248232d56b26
Alex Kim <alexkim943@gmail.com>
2024-04-23 21:26:05 +0900

    feb15

---
+++ codeforces/1015B.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def main():
+    n = int(sys.stdin.readline())
+    s = list(sys.stdin.readline().strip())
+    t = list(sys.stdin.readline().strip())
+    ans = []
+    for i in range(n):
+        if s[i] != t[i]:
+            idx = -1
+            for j in range(i+1, n):
+                if s[j] == t[i]:
+                    idx = j
+                    break
+            if idx == -1:
+                sys.stdout.write("-1\n")
+                return
+            for j in range(idx-1, i-1, -1):
+                s[j], s[j+1] = s[j+1], s[j]
+                ans.append(j)
+    sys.stdout.write(f"{len(ans)}\n")
+    for a in ans:
+        sys.stdout.write(f"{a+1} ")
+
+
+
+if __name__ == "__main__":
+    main()
